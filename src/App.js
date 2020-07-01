d5e49f2730aea26731276ecad60b628f26d4423f
Bryan Rhodunda <rhodunda@udel.edu>
2020-07-01 14:11:24 -0400

    added browerRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { BrowerRouter } from 'react-router-dom'
 
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
+      <BrowerRouter>
       <div className="App">
         <Blog />
       </div>
+      </BrowerRouter>
     );
   }
 }
